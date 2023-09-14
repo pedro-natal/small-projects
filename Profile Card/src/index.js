14a853b08a3a3bf80612823b749e1771e8429ad0
pedro-natal <pedropferreiranatal@gmail.com>
2023-09-14 17:16:38 -0300

    updated Profile Card

--- Profile Card/src/index.js
+++ Profile Card/src/index.js
@@ -1,17 +1,69 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./styles.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function App() {
+  return (
+    <div className="card">
+      <Avatar />
+      <div className="data">
+        <Intro />
+        {/* Should contain one Skill component
+        for each web dev skill that you have,
+        customized with props */}
+        <SkillList />
+      </div>
+    </div>
+  );
+}
+
+function Avatar() {
+  return (
+    <img className="avatar" src="avatar.png" alt="Pedro Ferreira Natal"></img>
+  );
+}
+
+function Intro() {
+  return (
+    <div>
+      <h1>Pedro Ferreira Natal</h1>
+      <p>
+        Atualmente, trabalhando em projetos web, utilizando HTML, CSS,
+        JavaScript, Bootstrap e React. Também possuo conhecimento em outras
+        “stacks”. Busco uma oportunidade de ingressar e me desenvolver em uma
+        empresa, aplicando meus conhecimentos técnicos e habilidades pessoais.
+        Sempre colocando a frente o trabalho equipe e o crescimento pessoal e
+        profissional.
+      </p>
+    </div>
+  );
+}
+
+function SkillList() {
+  return (
+    <div className="skill-list">
+      <Skill skill="HTML + CSS" emoji="🖼️" color="orange"></Skill>
+      <Skill skill="JavaScript" emoji="🌐" color="yellow"></Skill>
+      <Skill skill="Bootstrap" emoji="💻" color="red"></Skill>
+      <Skill skill="React" emoji="⚛️" color="purple"></Skill>
+    </div>
+  );
+}
+
+function Skill(props) {
+  return (
+    <div className="skill" style={{ backgroundColor: props.color }}>
+      <span>{props.skill}</span>
+      <span>{props.emoji}</span>
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
 root.render(
-  <React.StrictMode>
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
