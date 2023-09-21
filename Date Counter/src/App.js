5b8e5532a2f7cd072d33a4a56ac0bc475a85d495
pedro-natal <pedropferreiranatal@gmail.com>
2023-09-21 19:01:36 -0300

    added more states on project

--- Date Counter/src/App.js
+++ Date Counter/src/App.js
@@ -13,17 +13,33 @@ function Counter() {
   const [count, setCount] = useState(0);
   const [step, setStep] = useState(1);
   const date = new Date();
+
+  function handleReset() {
+    setCount(0);
+    setStep(1);
+  }
+
   date.setDate(date.getDate() + count);
   return (
     <div>
       <div>
-        <button onClick={() => setStep((s) => s - 1)}>-</button>
-        <span>Step: {step}</span>
-        <button onClick={() => setStep((s) => s + 1)}>+</button>
+        <input
+          type="range"
+          min="0"
+          max="10"
+          value={step}
+          onChange={(e) => setStep(Number(e.target.value))}
+        ></input>
+        <span>{step}</span>
       </div>
       <div>
         <button onClick={() => setCount((c) => c - step)}>-</button>
-        <span>Count: {count}</span>
+        <input
+          type="text"
+          placeholder="0"
+          value={count}
+          onChange={(e) => setCount(Number(e.target.value))}
+        ></input>
         <button onClick={() => setCount((c) => c + step)}>+</button>
       </div>
       <p>
@@ -36,6 +52,11 @@ function Counter() {
         </span>
         <span>{date.toDateString()}</span>
       </p>
+      {count !== 0 || step !== 1 ? (
+        <div>
+          <button onClick={handleReset}>Reset</button>
+        </div>
+      ) : null}
     </div>
   );
 }
